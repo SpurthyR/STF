eb2bef556985a740eafe37ba643596dd9e1cfb4c
SpurthyR <spurthyr@live.com>
2015-04-12 13:37:21 -0700

    First Commit

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,11 +6,26 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
+router.get('/',function(req,res,next){
+	res.render('index');
+});
+
+router.get('/article', function (req, res, next) {
   db.Article.findAll().success(function (articles) {
-    res.render('index', {
+    res.render('article', {
       title: 'Generator-Express MVC',
       articles: articles
     });
   });
+});
+
+router.get('/helloworld',function(req,res,next){
+	res.render('helloworld',{
+		title: 'Hello World'
+	});
+	
+});
+
+router.get('/createProposal.html', function(req,res, next){
+	res.render('createProposal');
 });
---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    
+    var navListItems = $('ul.setup-panel li a'),
+        allWells = $('.setup-content');
+
+    allWells.hide();
+
+    navListItems.click(function(e)
+    {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this).closest('li');
+        
+        if (!$item.hasClass('disabled')) {
+            navListItems.closest('li').removeClass('active');
+            $item.addClass('active');
+            allWells.hide();
+            $target.show();
+        }
+    });
+    $('ul.setup-panel li.active a').trigger('click');
+    
+    $("[id^='activate-step']").on('click', function(e) {
+        
+    e.preventDefault(); 
+        var currentId = $(this).attr('id').split('-')[2];
+        $('ul.setup-panel li:eq('+ (currentId -1) +')').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-' + currentId +'"]').trigger('click');
+        //$(this).remove();
+    }) 
+    
+    // $('#activate-step-2').on('click', function(e) {
+    //     $('ul.setup-panel li:eq(1)').removeClass('disabled');
+    //     $('ul.setup-panel li a[href="#step-2"]').trigger('click');
+    //     //$(this).remove();
+    // })
+
+    // $('#activate-step-3').on('click', function(e) {
+    //     $('ul.setup-panel li:eq(2)').removeClass('disabled');
+    //     $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+    //     //$(this).remove();
+    // })
+
+    // $('#activate-step-4').on('click', function(e) {
+    //     $('ul.setup-panel li:eq(3)').removeClass('disabled');
+    //     $('ul.setup-panel li a[href="#step-4"]').trigger('click');
+    //     //$(this).remove();
+    // }) 
+
+    // $('#activate-step-5').on('click', function(e) {
+    //     $('ul.setup-panel li:eq(4)').removeClass('disabled');
+    //     $('ul.setup-panel li a[href="#step-5"]').trigger('click');
+    //     //$(this).remove();
+    // })    
+});
